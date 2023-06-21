acfb7941b8299d401d9d5779129d98d3ab525ca9
Apprenant18 <apprenant18@macbook-pro-de-apprenant18.home>
2023-06-21 12:09:40 +0200

    first commit

---
+++ GitProject/GitProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitProjectApp.swift
+//  GitProject
+//
+//  Created by Apprenant18 on 21/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct GitProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
